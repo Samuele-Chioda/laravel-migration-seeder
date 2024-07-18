c218d61e21655c2b4a3c03865eb564c0021313dc
Samuele-Chioda <samuelechioda@gmail.com>
2024-07-18 17:42:11 +0200

    struttura index.blade.php e aggiunta style

--- resources/views/guest/pages/home.blade.php
+++ resources/views/guest/pages/home.blade.php
@@ -3,15 +3,5 @@
 @section('page-title', 'homepage')
 
 @section('main-content')
-    <h1>Lista dei treni in partenza in giornata:</h1>
-    <section>
-        @foreach ($train as $trenoSingolo)
-            <article>
-                <h2>
-                    Azienda: {{ $trenoSingolo->azienda }}
-                </h2>
-            </article>
-        @endforeach
-    </section>
-    @endforeach
+
 @endsection
--- resources/views/train/index.blade.php
+++ resources/views/train/index.blade.php
@@ -3,8 +3,22 @@
 @section('page-title', 'train index')
 
 @section('main-content')
-    <h1>
-        Homepage
-    </h1>
-    @dump($train)
+    <h1>Lista dei treni in partenza in giornata:</h1>
+    <section class="cards-treni">
+        @foreach ($train as $trenoSingolo)
+            <article>
+                <h2>
+                    Azienda: {{ $trenoSingolo->azienda }}
+                </h2>
+                <h3>
+                    Stazione di partenza: {{ $trenoSingolo->stazione_di_partenza }}
+                </h3>
+                <div>
+                    <p>Partenza prevista: {{ $trenoSingolo->orario_di_partenza }}</p>
+                    <p>Arrivo previsto: {{ $trenoSingolo->orario_di_arrivo }}</p>
+                    <p>Codice del treno: {{ $trenoSingolo->codice_treno }} </p>
+                </div>
+            </article>
+        @endforeach
+    </section>
 @endsection
