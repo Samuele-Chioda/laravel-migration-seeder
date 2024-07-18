c9d1dbb3c29df12ac6508109e5400611055b4caf
Samuele-Chioda <samuelechioda@gmail.com>
2024-07-18 15:44:46 +0200

    aggiunta parametri tabella train

--- database/migrations/2024_07_18_134217_create_train_table.php
+++ database/migrations/2024_07_18_134217_create_train_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('train', function (Blueprint $table) {
             $table->id();
+            $table->string('azienda', 50);
+            $table->string('stazione_di_partenza', 50);
+            $table->string('stazione_di_arrivo', 50);
+            $table->dateTime('orario_di_partenza');
+            $table->dateTime('orario_di_arrivo');
+            $table->integer('codice_treno');
+            $table->integer('numero_carrozze');
+            $table->boolean('in_orario');
+            $table->boolean('cancellato');
             $table->timestamps();
         });
     }
