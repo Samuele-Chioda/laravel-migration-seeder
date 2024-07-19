8ce04172b9285702aca5a0630c5d1e45d3a13916
Samuele-Chioda <samuelechioda@gmail.com>
2024-07-19 14:19:17 +0200

    style

--- resources/views/train/index.blade.php
+++ resources/views/train/index.blade.php
@@ -8,15 +8,21 @@
         @foreach ($train as $trenoSingolo)
             <article>
                 <h2>
-                    Azienda: {{ $trenoSingolo->azienda }}
+                    Azienda:<span> {{ $trenoSingolo->azienda }}</span>
                 </h2>
                 <h3>
-                    Stazione di partenza: {{ $trenoSingolo->stazione_di_partenza }}
+                    Stazione di partenza:<span> {{ $trenoSingolo->stazione_di_partenza }}</span>
+                </h3>
+                <h3>
+                    Stazione di arrivo:<span> {{ $trenoSingolo->stazione_di_arrivo }}</span>
                 </h3>
                 <div>
-                    <p>Partenza prevista: {{ $trenoSingolo->orario_di_partenza }}</p>
-                    <p>Arrivo previsto: {{ $trenoSingolo->orario_di_arrivo }}</p>
-                    <p>Codice del treno: {{ $trenoSingolo->codice_treno }} </p>
+                    <p>Partenza prevista:<span> {{ $trenoSingolo->orario_di_partenza }}</span></p>
+                    <p>Arrivo previsto:<span> {{ $trenoSingolo->orario_di_arrivo }}</span></p>
+                    <p>Codice del treno:<span> {{ $trenoSingolo->codice_treno }}</span> </p>
+                    <p>
+                        Numero delle carrozze del treno: <span>{{ $trenoSingolo->numero_carrozze }} </span>
+                    </p>
                 </div>
             </article>
         @endforeach
